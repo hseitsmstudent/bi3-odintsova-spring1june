fae3e532442317a7517a0849078fa0695eda849d
hseitsmstudent <hse.itsm.student@gmail.com>
2017-04-27 15:03:07 +0300

    Initial commit

---
+++ src/main/java/com/example/Bi3OdintsovaSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bi3OdintsovaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bi3OdintsovaSpringApplication.class, args);
+	}
+}
